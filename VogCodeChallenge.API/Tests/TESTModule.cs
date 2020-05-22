9db89744026e557b61420cab1e43b2eb60340c0f
Stefan <jovanovicstefan69@gmail.com>
2020-05-22 16:21:34 -0600

    question5

---
+++ VogCodeChallenge.API/Tests/TESTModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Tests
+{
+    public class TESTModule
+    {
+        public static object Module(object testObject)
+        {
+            switch (testObject)
+            {
+                case 1:
+                case 2:
+                    return doubler((int)testObject);
+                case int value when value >= 3:
+                    return tripler(value);
+                case float floatValue when floatValue == 1.0f:
+                    return leveler(floatValue);
+                default:
+                    return testObject;
+            }
+
+
+            int doubler(int amount) => amount * 2;
+            int tripler(int amount) => amount * 3;
+            double leveler(double amount) => amount - 0.1f * 10f;
+
+        }
+    }
+}
--- VogCodeChallenge.API/Tests/TestPlaygroundClass.cs
+++ VogCodeChallenge.API/Tests/TestPlaygroundClass.cs
@@ -11,7 +11,26 @@ namespace VogCodeChallenge.API.Tests
         public TestPlaygroundClass()
         {
             TestCreateEntities();
+            TestMod();
         }
+
+        public void TestMod()
+        {
+            var oneAmount = TESTModule.Module(1);
+            var doubleAmount = TESTModule.Module(2);
+            var doubleAmount2 = TESTModule.Module(2.0);
+
+            var tripleAmount = TESTModule.Module(3);
+
+            var floatAmount = TESTModule.Module(1.0f);
+            var floatAmount2 = TESTModule.Module(1.0000000000f);
+            var floatAmount3 = TESTModule.Module(1.0000001f);
+
+            //float amount4 should not return 0, use double instead of float in the TESTModule code for more accurate results
+            var floatAmount4 = TESTModule.Module(1.00000001f);
+
+        }
+
 
         public void TestCreateEntities()
         {
