5cfc023be09d1631fb9d1685bb163566f6da3565
Stefan <jovanovicstefan69@gmail.com>
2020-05-22 17:16:20 -0600

    question8

---
+++ ConsoleApp2/QuestionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            foreach(string name in NamesList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+
+}
